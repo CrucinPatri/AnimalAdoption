2171c9f2f74b2d0c931e1cb0a09049d9d5505f8d
Aministrator <crucin.patricia@yahoo.com>
2026-09-15 19:12:38 +0300

    ServiceAnimal

--- src/main/java/org/example/controller/ControllerAnimal.java
+++ src/main/java/org/example/controller/ControllerAnimal.java
@@ -1,7 +1,7 @@
 package org.example.controller;
 
 import org.example.model.Animal;
-import org.example.repository.RepositoryAnimal;
+import org.example.service.ServiceAnimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -11,22 +11,20 @@ import java.util.List;
 public class ControllerAnimal {
 
     @Autowired
-    private RepositoryAnimal repositoryAnimal;
+    private ServiceAnimal serviceAnimal;
 
-    //GET: Returneaza toate animalele
     @GetMapping
     public List<Animal> getAnimals() {
-        return repositoryAnimal.findAll();
+        return serviceAnimal.getAnimals();
     }
 
-    //POST: adauga un animal nou
     @PostMapping
     public Animal saveAnimal(@RequestBody Animal animal) {
-        return repositoryAnimal.save(animal);
+        return serviceAnimal.saveAnimal(animal);
     }
 
     @DeleteMapping("/{id}")
     public void deleteAnimal(@PathVariable Long id) {
-        repositoryAnimal.deleteById(id);
+        serviceAnimal.deleteById(id);
     }
 }
