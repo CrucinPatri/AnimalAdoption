f8f47d05258907edadd399ee7d498aa5217b5a04
Aministrator <crucin.patricia@yahoo.com>
2026-09-15 19:18:39 +0300

    ServiceAnimal

---
+++ src/main/java/org/example/service/ServiceAnimal.java
@@ -0,0 +1,26 @@
+package org.example.service;
+
+import org.example.model.Animal;
+import org.example.repository.RepositoryAnimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceAnimal {
+    @Autowired
+    private RepositoryAnimal repositoryAnimal;
+
+    public List<Animal> getAnimals() {
+        return repositoryAnimal.findAll();
+    }
+
+    public Animal saveAnimal(Animal animal) {
+        return repositoryAnimal.save(animal);
+    }
+
+    public void deleteById(Long id) {
+        repositoryAnimal.delete(id);
+    }
+}
