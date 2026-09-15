8112d6c6c173955eb2798a7aedc9760da1fb5de9
Aministrator <crucin.patricia@yahoo.com>
2026-09-15 18:53:37 +0300

    Conectare baza de date

--- src/main/java/org/example/controller/ControllerAnimal.java
+++ src/main/java/org/example/controller/ControllerAnimal.java
@@ -5,30 +5,28 @@ import org.example.repository.RepositoryAnimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+@RestController
+@RequestMapping("/api/animals")
 public class ControllerAnimal {
-    @RestController
-    @RequestMapping("/api/animals")
-    public class ControllerAnimals{
-        @Autowired
-        private RepositoryAnimal repositoryAnimal;
 
-        //GET: Returneaza toate animalele
-        @GetMapping
-        public List<Animal> getAnimals(){
-            return repositoryAnimal.findAll();
-        }
+    @Autowired
+    private RepositoryAnimal repositoryAnimal;
 
-        //POST: adauga un animal nou
-        @PostMapping
-        public Animal saveAnimal(@RequestBody Animal animal){
-            return repositoryAnimal.save(animal);
-        }
-
-        @DeleteMapping("/{id}")
-        public void deleteAnimal(@PathVariable Long id){
-            repositoryAnimal.deleteById(id);
-        }
+    //GET: Returneaza toate animalele
+    @GetMapping
+    public List<Animal> getAnimals() {
+        return repositoryAnimal.findAll();
     }
 
+    //POST: adauga un animal nou
+    @PostMapping
+    public Animal saveAnimal(@RequestBody Animal animal) {
+        return repositoryAnimal.save(animal);
+    }
 
+    @DeleteMapping("/{id}")
+    public void deleteAnimal(@PathVariable Long id) {
+        repositoryAnimal.deleteById(id);
+    }
 }
--- src/main/java/org/example/repository/RepositoryAnimal.java
+++ src/main/java/org/example/repository/RepositoryAnimal.java
@@ -4,6 +4,6 @@ import org.example.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface RepositoryAnimal extends JpaRepository<Animal,Integer> {
+public interface RepositoryAnimal extends JpaRepository<Animal,Long> {
 
 }
